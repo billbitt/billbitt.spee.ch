54158a202fe8a226e83e1fdd3e206f68a3b29673
bill bittner <bittner.w@gmail.com>
2018-03-16 01:14:32 -0700

    removed index.js

---
+++ client/client.js
@@ -0,0 +1,1 @@
+const { speechClient } = require('spee.ch');
--- server.js
+++ server.js
@@ -1,29 +1,20 @@
 // require the spee.ch package
 const speech = require('spee.ch');
+// get config files
+const { mysqlConfig, siteConfig, slackConfig } = require('./config')
+const pages = require('./pages');
 
-// create a new spee.ch server
-console.log('creating speech server');
-server = new speech();
-
-//configure the server
-const config = require('./config');
-server.startEngine(config);
-
-// get local pages
-server.getLocalPages('./pages');
-
-// // get local routes
-// server.getLocalRoutes('./routes');
-//
-// // get local models
-// server.getLocalModels('./models');
-
-// run speech
 try {
-    console.log('warming up the speech server');
-    server.warmupEngine();
-    console.log('starting speech server');
-    server.blastoff();
+    // create a new spee.ch server
+    const server = new speech.server();
+    // configure server
+    server.configureMysql(mysqlConfig);
+    server.configureSite(siteConfig);
+    server.configureSlack(slackConfig);
+    // initialize (creates the express app and configures it)
+    server.initialize();
+    // start the server (syncs db and
+    server.start();
 } catch (error) {
-    console.log('spee.ch server error:', error);
+    console.log('spee.ch server startup error:', error);
 }
