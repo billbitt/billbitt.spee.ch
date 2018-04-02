103ab0b36de0f4a3d048a995a56cd93ebae2f8d7
bill bittner <bittner.w@gmail.com>
2018-04-02 10:12:24 -0700

    renamed configureSiteDetails to configureSite

--- server.js
+++ server.js
@@ -7,20 +7,18 @@ const siteConfig = require('./config/siteConfig.js');
 const slackConfig = require('./config/slackConfig.js');
 
 try {
-    console.log(`Current directory (index.js): ${process.cwd()}`);
     // create a new spee.ch server
     const server = new Speech.Server();
-    // configure server
+    // configure the server
     server.configureLogger(loggerConfig);
     server.configureMysql(mysqlConfig);
-    server.configureSiteDetails(siteConfig);
+    server.configureSite(siteConfig);
     server.configureSlack(slackConfig);
-    server.configureClientBundle();
     server.configureModels();
     server.configureRoutes();
-    // initialize (creates the express app and configures it)
+    // create the express app and configure it
     server.initialize();
-    // start the server (syncs db and
+    // sync the db and start express app listening on the configured port
     server.start();
 } catch (error) {
     console.log('server startup error:', error);
